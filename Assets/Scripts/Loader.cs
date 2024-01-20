e73120b726354e7cd2fcdb3025b0d5f4d1d95686
GrafEnters <graf.enters@gmail.com>
2024-01-20 14:43:11 +0300

    Loading scene

--- Assets/Scripts/Loader.cs
+++ Assets/Scripts/Loader.cs
@@ -1,30 +1,46 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Loader : MonoBehaviour {
     [SerializeField]
     private GameObject _loadingPanel;
-    
+
     [SerializeField]
     private ColorsConfig _colorsConfig;
 
+    [SerializeField]
+    private bool _isOnLoading;
+    
     public static ColorsConfig ColorsConfig;
     public static TasksManager TasksManager;
     public static UITasksPanel UITasksPanel;
 
     private void Awake() {
+        _isOnLoading = SceneManager.GetActiveScene().name == "LoadingScene";
         StartCoroutine(LoadingCoroutine());
     }
 
     private IEnumerator LoadingCoroutine() {
         ColorsConfig = _colorsConfig;
-        _loadingPanel.SetActive(true);
+        SetLoadingPanel(true);
         TasksManager = FindObjectOfType<TasksManager>();
+
         yield return TasksManager.Loading();
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("MainScene", LoadSceneMode.Additive);
+        while (!asyncLoad.isDone) {
+            yield return null;
+        }
 
         UITasksPanel = FindObjectOfType<UITasksPanel>();
         yield return UITasksPanel.Loading();
-        
-        _loadingPanel.SetActive(false);
+        SceneManager.UnloadSceneAsync("LoadingScene");
+        SetLoadingPanel(false);
+    }
+
+    private void SetLoadingPanel(bool isActive) {
+        if (_loadingPanel) {
+            _loadingPanel.SetActive(isActive);
+        }
     }
 }
